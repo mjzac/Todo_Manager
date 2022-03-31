3ad3ff1d1d201d74ba19572c85a1396333f8febc
Melson Zacharias <jmelson7@gmail.com>
2022-03-31 09:10:42 +0530

    Add ability to update todo

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,6 +1,7 @@
 # todos_controller.rb
 class TodosController < ApplicationController
   skip_before_action :verify_authenticity_token
+
   def index
     render plain: Todo.order(:due_date).map { |todo| todo.to_pleasant_string }.join("\n")
   end
@@ -20,6 +21,16 @@ class TodosController < ApplicationController
       completed: false,
     )
     response_text = "Hey, your new todo is created with the id #{new_todo.id}"
+    render plain: response_text
+  end
+
+  def update
+    id = params[:id]
+    completed = params[:completed]
+    todo = Todo.find(id)
+    todo.completed = completed
+    todo.save!
+    response_text = "Updated todo completed status for #{todo.id} to #{completed}"
     render plain: response_text
   end
 end
