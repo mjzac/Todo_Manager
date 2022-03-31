6d4855cf514505464505299f922d9bba085e82d7
Melson Zacharias <jmelson7@gmail.com>
2022-03-31 09:39:34 +0530

    Add ability to display user details

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,10 @@ class UsersController < ApplicationController
   def index
     render plain: User.order(:name).map { |user| user.to_pleasant_string }.join("\n")
   end
+
+  def show
+    id = params[:id]
+    user = User.find(id)
+    render plain: user.to_pleasant_string
+  end
 end
