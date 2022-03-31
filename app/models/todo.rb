061e4c6d70919c14cc5a32997a2b35f686294f4f
Melson Zacharias <jmelson7@gmail.com>
2022-03-31 09:43:11 +0530

    Fix formatting

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,6 +1,6 @@
 class Todo < ActiveRecord::Base
   def to_pleasant_string
-    is_completed = completed ? "[x]": "[]"
+    is_completed = completed ? "[x]" : "[]"
     "#{id}. #{due_date.to_fs} #{todo_text} #{is_completed}"
   end
 end
