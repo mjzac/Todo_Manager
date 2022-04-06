eb8e71853be8b7ab77b1d6185b8edfb081f301fd
Melson Zacharias <jmelson7@gmail.com>
2022-04-06 06:31:49 +0530

    Add User - Todo relationship

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,4 +1,6 @@
 class Todo < ActiveRecord::Base
+  belongs_to :user
+
   def to_pleasant_string
     is_completed = completed ? "[x]" : "[]"
     "#{id}. #{due_date.to_fs} #{todo_text} #{is_completed}"
