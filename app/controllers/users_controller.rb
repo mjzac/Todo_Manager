2f150a8ec459ed49d6dd3158bc1d2ac130dad907
Melson Zacharias <jmelson7@gmail.com>
2022-04-08 07:14:07 +0530

    Validate user before sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,13 +21,18 @@ class UsersController < ApplicationController
     last_name = params[:last_name]
     email = params[:email]
     password = params[:password]
-    new_user = User.create!(
+    new_user = User.new(
       first_name: first_name,
       last_name: last_name,
       email: email,
       password: password,
     )
-    redirect_to root_path
+    if new_user.save
+      redirect_to root_path
+    else
+      flash[:error] = new_user.errors.full_messages.join(", ")
+      redirect_to new_user_path
+    end
   end
 
   def update
